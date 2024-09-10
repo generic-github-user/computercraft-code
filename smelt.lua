dfdd4212a63d789c30d165ac2c5bc8a5030a9521
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2024-09-10 00:39:43 -0400

    2024-09-10_00:39:43

--- smelt.lua
+++ smelt.lua
@@ -1,14 +1,14 @@
 Graph = {}
 
-function Graph:new()
-    local g = { nodes = List:new(), edges = List:new() }
+function Graph:new(directed)
+    local g = { nodes = List:new(), edges = List:new(), directed = directed }
     setmetatable(g, self)
     self.__index = self
     return g
 end
 
-function Graph:from(nodes, edges)
-  local g = Graph:new()
+function Graph:from(nodes, edges, directed)
+  local g = Graph:new(directed)
   g.nodes = List:from(nodes)
   g.edges = List:from(edges)
   return g
@@ -129,6 +129,8 @@ function s_contains(s)
 end
 
 function Graph:dijkstra(n)
+  assert(not self.directed)
+
   local dist = {}
   local prev = {}
   -- local r = self.nodes:length()
@@ -172,9 +174,121 @@ function Graph:shortest_path(a, b)
   return recover_path(p, a, b)
 end
 
+function VirtualChest:new(shape)
+  assert(shape.dim.x == 1 or shape.dim.y == 1)
+  local c = { shape = shape }
+  setmetatable(c, self)
+  self.__index = self
+  return c
+end
+
+function derive_eq(properties)
+  return function (a, b)
+    return properties:all(function (i) a[i] == b[i] end)
+  end
+end
+
+function Rect:new(pos, dims)
+  local r = { pos = pos, dims = dims }
+  setmetatable(r, self)
+  self.__index = self
+  self.__eq = derive_eq(List:from({ "pos", "dims" }))
+  return r
+end
+
+function Rect:normalize()
+  local r = Rect(self.pos, self.dims)
+  for _, a in List:from({"x", "y", "z"}):iter() do
+    if r.dims[a] < 0 then
+      r.pos[a] = r.pos[a] + r.dims[a] + 1
+      r.dims[a] = -r.dims[a]
+    end
+  end
+  return r
+end
+
+function Rect:bounds()
+  return self.pos, vec.new(self.pos.x + self.dims.x - 1, self.pos.y + self.dims.y - 1, self.pos.z + self.dims.z - 1)
+end
+
+function Rect:bound_ranges()
+  local a, b = self.bounds()
+  return Range(a.x, b.x), Range(a.y, b.y), Range(a.z, b.z)
+end
+
+function Rect:intersect(a, b)
+  local a_bounds = a.bound_ranges()
+  local b_bounds = b.bound_ranges()
+  return Rect:from_bounds(Range:intersection)
+
+function Range:new(a, b)
+  local r = {a = a, b = b}
+  setmetatable(r, self)
+  self.__index = self
+  return r
+end
+
+function Range:intersection(a, b)
+
+function List:foreach(f)
+  for _, x in self:iter() do
+    f(x)
+  end
+end
+
+function List:extend(xs)
+  return xs:foreach(self:append)
+end
+
+function List:concat_n(xs)
+  local l = List:new()
+  xs:foreach(l:extend)
+  return l
+end
+
+function VirtualChest:n_slots()
+  return self.shape.dim.x * self.shape.dim.y * self.shape.dim.z * 27 * 2
+end
+
+function furnace_index(i)
+  return vec.new(i % width, 1, (i // width) * 2 + 1)
+end
+
+width = 10
+height = 2
+pos = {
+  inputs = {},
+  fuel = {},
+  outputs = {}
+}
+fuel_limit = math.ceil(width * height * 3 * 1.5)
+
+function distribute_inputs()
+
+end
+
+function table_size(t)
+  return List:from(pairs(t)):length()
+end
+
+function table_product(t)
+  
+end
+
+function move_xyz(delta)
+    print("following vector: " .. serialize_vector(delta))
+
+    move_x(delta.x)
+    move_y(delta.y)
+    move_z(delta.z)
+end
+
+function travel()
+
 function main()
   local g = Graph:from({ 1, 2, 3, 4, 5 },
-    { edge(1, 2), edge(2, 3), edge(3, 4), edge(4, 5), edge(3, 5) })
+    { edge(1, 2), edge(2, 3), edge(3, 4), edge(4, 5), edge(3, 5) },
+    false)
   print(g)
   print(g:show())
   print(g:neighbors(2):show())
@@ -182,9 +296,10 @@ function main()
   print(dump(dist))
   print(dump(prev))
   print(g:shortest_path(1, 4):show())
+  print(Rect(vec.new(1, 2, 3), vec.new(4, 5, 6)) == Rect(vec.new(1, 2, 3), vec.new(4, 5, 6)))
   
   -- while true do
-
+    -- distribute_inputs()
   -- end
 end
 
